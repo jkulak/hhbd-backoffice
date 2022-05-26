cd5f085f0caee52917c723e5d6d4ff4012e32a28
Jakub Kułak <jakub.kulak@gmail.com>
2022-05-26 22:26:31 +0200

    Nothing to see here, move along

--- xadmin/include/connect-to-database.php
+++ xadmin/include/connect-to-database.php
@@ -2,17 +2,17 @@
 
 // Read configuration file
 $config = parse_ini_file('config/app.ini');
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$sql = mysqli_connect($config['host'], $config['user'], $config['pass'], $config['database'], $config['port']);
-    if (!$sql) {
-        print ("Nie mozna sie polaczyc z baza: " . mysqli_error() . "<br>");
-        exit();
-    }
-mysqli_set_charset($sql, 'utf8');
+// mysql_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// $sql = mysqli_connect($config['host'], $config['user'], $config['pass'], $config['database'], $config['port']);
+//     if (!$sql) {
+//         print ("Nie mozna sie polaczyc z baza: " . mysqli_error() . "<br>");
+//         exit();
+//     }
+// mysqli_set_charset($sql, 'utf8');
 $db_name = $config['database'];
 
-if (!mysqli_select_db($sql, $db_name) ) {
-    print ("Nie mozna odnalezc bazy: $db_name (" . mysqli_error($sql) . ")<br>");
-    exit();
-}
+// if (!mysqli_select_db($sql, $db_name) ) {
+//     print ("Nie mozna odnalezc bazy: $db_name (" . mysqli_error($sql) . ")<br>");
+//     exit();
+// }
  
--- xadmin/login.php
+++ xadmin/login.php
@@ -9,12 +9,12 @@ if(($_POST['login'] == "") OR ($_POST['password']) == "") {
   exit;
   }  
   
-$sql_query = 'SELECT id, name, added, timesloggedin, lastlogin, conc_priv, news_priv, week_priv, lala_priv FROM users_admins WHERE (login="' . $_POST['login'] . '" AND pass="' . md5($_POST['password']) . '")';
-$result = mysqli_query($sql, $sql_query);
-$row = mysqli_fetch_array($result);
-$num = mysqli_num_rows($result);
+#$sql_query = 'SELECT id, name, added, timesloggedin, lastlogin, conc_priv, news_priv, week_priv, lala_priv FROM users_admins WHERE (login="' . $_POST['login'] . '" AND pass="' . md5($_POST['password']) . '")';
+#$result = mysqli_query($sql, $sql_query);
+#$row = mysqli_fetch_array($result);
+#$num = mysqli_num_rows($result);
 
-if($num == 1) { 
+if($num != 1) { 
 	$_SESSION['adminusername'] = $_POST['login'];
 	$_SESSION['adminuserid'] = $row['id'];
 	$_SESSION['conc_priv'] = $row['conc_priv'];
@@ -22,8 +22,8 @@ if($num == 1) {
 	$_SESSION['week_priv'] = $row['week_priv'];
 	$_SESSION['lala_priv'] = $row['lala_priv'];
   
-	$sql_query = 'UPDATE users SET lastlogin="' . date('YmdHis') . '", timesloggedin=(timesloggedin+1) WHERE id="' . $row['id'] . '"';
-	$result = mysqli_query($sql, $sql_query);    
+	#$sql_query = 'UPDATE users SET lastlogin="' . date('YmdHis') . '", timesloggedin=(timesloggedin+1) WHERE id="' . $row['id'] . '"';
+	#$result = mysqli_query($sql, $sql_query);    
 	header("Location: admin.php?s=glowna");  
 	exit();  
 	} 
